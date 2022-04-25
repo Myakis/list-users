5268084427c729f61fac21b70e4640e6400e841b
Myakish <myakishevandrey@gmail.com>
2022-04-25 11:21:12 +1000

    project initiation

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='container'>Всем fgsggприdgвет</h1>gdgd
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const babelOptions = preset => {
+  const opts = {
+    presets: ['@babel/preset-env'],
+    // plugins: ['@babel/plugin-proposal-class-properties'],
+  };
+  if (preset) {
+    opts.presets.push(preset);
+  }
+  return opts;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: ['@babel/polyfill', './index.tsx'],
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  devServer: {
+    port: 4200,
+    hot: isDev,
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    alias: {},
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      minify: isProd,
+      inject: 'body',
+      scriptLoading: 'blocking',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader', options: babelOptions() },
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader', options: babelOptions('@babel/preset-typescript') },
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader', options: babelOptions('@babel/preset-react') },
+      },
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+};
